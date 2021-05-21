17da8d0a6bedb32f91fd701f1abcf173ec8d3cd2
Ubaid Rana <ubaidrana45@gmail.com>
2021-05-21 11:50:25 +0200

    Added migrations with many relationships

--- SamuraiApp.Data/SamuraiContext.cs
+++ SamuraiApp.Data/SamuraiContext.cs
@@ -3,16 +3,30 @@ using SamuraiApp.Domain;
 
 namespace SamuraiApp.Data
 {
-    public class SamuraiContext:DbContext
+    public class SamuraiContext : DbContext
     {
         public DbSet<Samurai> Samurais { get; set; }
         public DbSet<Quote> Quotes { get; set; }
+        public DbSet<Battle> Battles { get; set; }
+
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer(
                 "Data Source=localhost; Initial Catalog=SamuraiAppDataFirstLook; User ID=sa; Password=MyComplex!@#$");
-            
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+               modelBuilder.Entity<Samurai>()
+                .HasMany(s => s.Battles)
+                .WithMany(b => b.Samurais)
+                .UsingEntity<BattleSamurai>
+                 (bs => bs.HasOne<Battle>().WithMany(),
+                  bs => bs.HasOne<Samurai>().WithMany())
+                .Property(bs => bs.DateJoined)
+                .HasDefaultValueSql("getdate()");
+
         }
     }
 }
